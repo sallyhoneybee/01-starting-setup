4536e7193d38d01fe879ba58fc0218584008fb4b
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-04-13 14:31:39 +0800

    removed demo button and event listener

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -9,13 +9,6 @@ function ExpenseItem(props) {
   // title is the value and setTitile is the function
   const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    // function will get react to evaluate and queue the setTitle in the function component
-
-    setTitle("updated!");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -23,8 +16,6 @@ function ExpenseItem(props) {
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      {/* only point to clickHandler and not pass it because if we clickHandler() it will be called once the JSX code block is being evaluated and not only when we click the button */}
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }

5979e15dcdf7754056c839869f643adca2182897
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-04-13 14:34:43 +0800

    Passed item title via porps fromm expenses to expense items

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,18 +4,17 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
-// Stateless component
+// Stateless/ presentational/ dumb component
 
 function ExpenseItem(props) {
   // assign an initial value for the state passed as an argument that will only evaluated once
   // title is the value and setTitile is the function
-  const [title, setTitle] = useState(props.title);
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
     </Card>

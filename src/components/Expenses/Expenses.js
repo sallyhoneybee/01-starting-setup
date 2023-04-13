281fe527f00cf2353efdff767b44e29baaa5a587
Summer Chew Jia Xing <summerchewjx@gmail.com>
2023-04-14 00:18:42 +0800

    Applied logic in Expenses so ExpenseFilter actually filters the different expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,17 +5,21 @@ import Card from "../UI/Card";
 import "./Expenses.css";
 
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState("");
+  const [filteredYear, setFilteredYear] = useState("2022");
 
   const selectedYearHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
+  };
+
+  const yearFilter = (expenseArray) => {
+    return String(expenseArray.date.getFullYear()) === filteredYear;
   };
 
   return (
     <div>
       <ExpensesFilter onSelectedYear={selectedYearHandler} />
       <Card className="expenses">
-        {props.expenses.map((expense) => (
+        {props.expenses.filter(yearFilter).map((expense) => (
           <ExpenseItem
             key={expense.id}
             title={expense.title}
